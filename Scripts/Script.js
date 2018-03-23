a6d761cebd4efb88192b8f9555197658ff660288
BryceLikesDogs <36935978+BryceLikesDogs@users.noreply.github.com>
2018-03-23 14:06:15 +1300

    Not working yet
    Doesnt work yet but is close

---
+++ Scripts/Script.js
@@ -0,0 +1,5 @@
+function switchButton() {
+  console.log(document.getElementsByClassName('landing-page').length)
+  document.getElementsByClassName('landing-page').style.display = 'none'
+  document.getElementsByClassName('switch-page').style.display = 'block'
+}
