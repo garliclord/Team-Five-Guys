83f9c331cdbbbd7207bf519dfacd2b8a5b4ec9ea
BryceLikesDogs <36935978+BryceLikesDogs@users.noreply.github.com>
2018-03-26 12:45:59 +1300

    Buttons working
    Buttons will now correctly show/hide pages. Also added in cancel buttons to bring page back to the landing page

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -1,5 +1,23 @@
 function switchButton() {
-  console.log(document.getElementsByClassName('landing-page').length)
-  document.getElementsByClassName('landing-page').style.display = 'none'
-  document.getElementsByClassName('switch-page').style.display = 'block'
+  changeDisplay('landing-page', 'none')
+  changeDisplay('switch-page', 'block')
+}
+
+function databaseButton() {
+  changeDisplay('landing-page', 'none')
+  changeDisplay('database-page', 'block')
+}
+
+function cancelButton() {
+  changeDisplay('database-page', 'none')
+  changeDisplay('switch-page', 'none')
+  changeDisplay('landing-page', 'block')
+}
+
+// Enter the clas name you want to select, and the value you want to change display to
+function changeDisplay (name, value) {
+  var elem = document.getElementsByClassName(name)
+  for (var i = 0; i < elem.length; i++) {
+    elem[i].style.display = value
+  }
 }
