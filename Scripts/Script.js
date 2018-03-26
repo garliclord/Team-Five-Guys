d122581d8ac48dd3e259d4c22de1d1044c39177d
BryceLikesDogs <36935978+BryceLikesDogs@users.noreply.github.com>
2018-03-26 14:03:29 +1300

    Switch page updated
    Switch page is now able to change the current user and the display is updated, also tidied up the js code a bit and split into more functions, added comments to everything to make it more maintainable and added notes for future expansion

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -1,20 +1,56 @@
-function switchButton() {
+// To be called when the page loads, gets the current user and displays it
+function startUpFunction () {
+  var currentUser = loadCurrentUser()
+  displayCurrentUser(currentUser)
+}
+
+// Gets the current user from the backend
+function loadCurrentUser () {
+  var username = 'Bryce'
+  return username
+}
+
+// Changes the h1 at the top of the screen to display the current user
+function displayCurrentUser (userName) {
+  document.getElementById('currentUser').innerHTML = 'Current user: ' + userName
+}
+
+// To be called when the switch button is pressed
+// Displays the switch page
+function switchButton () {
   changeDisplay('landing-page', 'none')
   changeDisplay('switch-page', 'block')
 }
 
-function databaseButton() {
+// To be called when the database button is pressed
+// Displays the database page
+function databaseButton () {
   changeDisplay('landing-page', 'none')
   changeDisplay('database-page', 'block')
 }
 
-function cancelButton() {
+// To be called when the cancel button is pressed, changes the display of all page divs to 'none'
+// and turns the landing-page div display to 'block'
+function cancelButton () {
   changeDisplay('database-page', 'none')
   changeDisplay('switch-page', 'none')
   changeDisplay('landing-page', 'block')
 }
 
-// Enter the clas name you want to select, and the value you want to change display to
+// To be called when the user clicks 'Confirm' on the switch page
+// Will take the data inputted into the 'username' field, change the current user to it
+// And also call the displayCurrentUser() function
+function switchConfirmButton () {
+  var name = document.getElementById('usernameInput').value
+  document.getElementById('usernameInput').value = ''
+  // In the future this will also call the fucntion whcih rewrites and saves the current username, but that isn't made yet
+  var currentUser = name
+  displayCurrentUser(currentUser)
+  changeDisplay('switch-page', 'none')
+  changeDisplay('landing-page', 'block')
+}
+
+// Enter the class name you want to select, and the value you want to change display to
 function changeDisplay (name, value) {
   var elem = document.getElementsByClassName(name)
   for (var i = 0; i < elem.length; i++) {
