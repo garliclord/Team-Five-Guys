a6141ab6d5aaf93956fcc8b36fb1746a7eb78e95
BryceLikesDogs <36935978+BryceLikesDogs@users.noreply.github.com>
2018-04-05 13:56:26 +1200

    finalised button trasitions

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -1,8 +1,17 @@
-// //Only dusplaying the os versions that are relevent, doesn't work yet...
-// document.querySelector("#ios-btn").addEventListener("click", () => {
-//   console.log("hi")
-//   $(".ios-os").prop('disabled', true);
-// })
+// To be used to hide the os buttons and reset the active property
+function hide(id) {
+  document.querySelectorAll(id).forEach((element) => {
+    element.classList.add("hidden");
+    element.classList.remove("active");
+  })
+}
+
+// to be used to show the os buttons which just remove the 'hidden' class
+function show(id) {
+  document.querySelectorAll(id).forEach((element) => {
+    element.classList.remove("hidden");
+  })
+}
 
 // transition button logic
 function clickButton(id, from, to) {
@@ -11,6 +20,25 @@ function clickButton(id, from, to) {
     document.querySelector(to).classList.toggle("hidden");
   });
 };
+
+// Displays the os versions for the ios OS
+document.querySelector("#ios-btn").addEventListener("click", () => {
+  hide(".android-os");
+  show(".ios-os");
+ })
+
+// Displays the os versions for the android OS
+document.querySelector("#android-btn").addEventListener("click", () => {
+  hide(".ios-os");
+  show(".android-os");
+ })
+
+// Hides all os choices when other is pressed
+document.querySelector("#other-btn").addEventListener("click", () => {
+ hide(".ios-os");
+ hide(".android-os");
+})
+
 // landing page
 clickButton("#search-btn", ".landing-page", ".display-page");
 
