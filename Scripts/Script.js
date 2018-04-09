b4b823c20f25dfccb97a5c38a914ec61c9287923
BryceLikesDogs <36935978+BryceLikesDogs@users.noreply.github.com>
2018-04-09 14:02:46 +1200

    Improved dynamicability of the buttons

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -13,52 +13,59 @@ function show(id) {
   });
 };
 
-// transition button logic
-function clickButton(id, from, to) {
-  document.querySelector(id).addEventListener("click", () => {
-    document.querySelector(from).classList.toggle("hidden");
-    document.querySelector(to).classList.toggle("hidden");
+// Deselects the os-all option when another button is pressed
+document.querySelectorAll('.os-non-all').forEach((element) => {
+  element.addEventListener("click", () => {
+    document.getElementById('os-all-btn').classList.remove("active")
   });
-};
+});
+
+// Deseletcs the other options when os-all-btn is pressed
+document.getElementById('os-all-btn').addEventListener("click", () => {
+  document.querySelectorAll('.os-non-all').forEach((element) => {
+    element.classList.remove("active")
+  });
+});
+
+// Deselects the dgrade-all option when another button is pressed
+document.querySelectorAll('.dgrade-non-all').forEach((element) => {
+  element.addEventListener("click", () => {
+    document.getElementById('dgrade-all-btn').classList.remove("active")
+  });
+});
+
+// Deseletcs the other options when dgrade-all-btn is pressed
+document.getElementById('dgrade-all-btn').addEventListener("click", () => {
+  document.querySelectorAll('.dgrade-non-all').forEach((element) => {
+    element.classList.remove("active")
+  });
+});
+
 
 // Displays the os versions for the ios OS
 document.querySelector("#ios-btn").addEventListener("click", () => {
   hide(".android-os");
   show(".ios-os");
+  document.getElementById("os-all-btn").classList.add("active");
 });
 
 // Displays the os versions for the android OS
 document.querySelector("#android-btn").addEventListener("click", () => {
   hide(".ios-os");
   show(".android-os");
+  document.getElementById("os-all-btn").classList.add("active");
 });
 
 // Hides all os choices when other is pressed
 document.querySelector("#other-btn").addEventListener("click", () => {
  hide(".ios-os");
  hide(".android-os");
+ document.getElementById("os-all-btn").classList.add("active");
 });
 
-// landing page
-clickButton("#search-btn", ".landing-page", ".display-page");
-clickButton("#add-device-btn", ".landing-page", ".add-device-page");
-
-// add device page
-clickButton("#add-device-page-save-btn", ".add-device-page", ".landing-page");
-clickButton("#add-device-page-back-btn", ".add-device-page", ".landing-page");
-
-// display page
-clickButton("#display-page-back-btn", ".display-page", ".landing-page");
-clickButton("#info-btn", ".display-page", ".info-page");
-clickButton("#assign-btn", ".display-page", ".assign-overlay");
-
-// info page
-clickButton("#info-page-edit-btn", ".info-page", ".edit-page");
-clickButton("#info-page-back-btn", ".info-page", ".display-page");
-
-// assign overlay
-clickButton("#assign-overlay-back-btn", ".assign-overlay", ".display-page");
-
-// edit page
-clickButton("#edit-page-save-btn", ".edit-page", ".info-page");
-clickButton("#edit-page-back-btn", ".edit-page", ".info-page");
+// Hides all os choices when all is pressed
+document.querySelector("#all-btn").addEventListener("click", () => {
+ hide(".ios-os");
+ hide(".android-os");
+ document.getElementById("os-all-btn").classList.add("active");
+});
